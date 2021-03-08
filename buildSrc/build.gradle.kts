d33de8aa0981ef9ea9b8e996908c879eabc00834
anastasiia.spaseeva <anastasiiia.spaseeva@jetbrains.com>
2021-03-08 17:21:11 +0100

    Patch build script to use kotlin snapshot version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,10 +16,12 @@ repositories {
         maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2")
         maven("https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/kotlin-eap")
         maven("https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/kotlin-dev")
+        maven( "file://" + rootProject.getProperties()["projectDir"] + "/../compiler/maven" )
     } else {
         maven("https://plugins.gradle.org/m2")
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
         maven("https://dl.bintray.com/kotlin/kotlin-dev")
+        maven("file://" + rootProject.getProperties()["projectDir"] + "/../compiler/maven" )
     }
 
     if (buildSnapshotTrain) {
